3d65f77d908f901df48691a01d3e4de1b2bd8d9f
chen6wx <chen6wx@dukes.jmu.edu>
2022-10-10 18:36:43 -0400

    working

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -4,3 +4,20 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+function outputTable() {
+   let subtotal = 0;
+   for (let item of cart) {
+      let total = calculateTotal(item.quantity, item.product.price);
+      subtotal += total;
+      outputCartRow(item, total);
+   }
+   let tax = subtotal * tax_rate;
+   let grandtotal = subtotal + tax;
+   let shipping = 40
+   if (subtotal > shipping_threshold) {
+      shipping = 0
+   }
+
+   outputTotals(subtotal, tax, grandtotal, shipping);
+}
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,38 @@
 /* define your functions here */
+function calculateTotal(quantity, price) {
+    return quantity * price;
+}
 
+function outputCartRow(item, total) {
+    // const {
+    //     product: {title, filename, price},
+    //     quantity,
+    // } = item;
+    const row = `<tr>
+                    <td><img src="images/${item.product.filename}"></td>
+                    <td>${item.product.title}</td>
+                    <td>${item.quantity}</td>
+                    <td>${"$" + item.product.price.toFixed(2)}</td>
+                    <td>${"$" + total.toFixed(2)}</td>
+                </tr>`
+    document.write(row)
+}
 
-
-
-
-
-
-
-        
+function outputTotals(subtotal, tax, grandtotal, shipping) {
+    const totals = `<tr class="totals">
+                     <td colspan="4">Subtotal</td>
+                     <td>${"$" + subtotal.toFixed(2)}</</td>
+                  </tr>
+                  <tr class="totals">
+                     <td colspan="4">Tax</td>
+                     <td>${"$" + tax.toFixed(2)}</td>
+                  </tr>
+                  <tr class="totals">
+                     <td colspan="4">Shipping</td>
+                     <td>${"$" + shipping}</td>
+                  </tr> <tr class="totals">
+                     <td colspan="4" class="focus">Grand Total</td>
+                     <td class="focus">${"$" + grandtotal}</td>
+                  </tr>`
+    document.write(totals)
+}   
